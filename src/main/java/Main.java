beb80e9fad25d234aa7dbfc6409c7b16b03e2fc8
Melak <km250542@ncr.com>
2023-03-20 17:43:46 +0100

    Documentation pt.1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,11 @@ import classes.StarterPack;
 
 public class Maine
 {
+    /*
+    *        !IMPORTANT!
+    * This application logic works correct always but is displayed correct only
+    * when is run on Windows command line or IOS/Linux terminal.
+     */
     public static void main(String[] args) throws Exception
     {
                StarterPack starterPack = new StarterPack();
